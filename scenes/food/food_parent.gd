7ca849da43492a66bcdb8063aa4ecdf6e4f02436
murilo-marian <murilomarian.mm@gmail.com>
2024-06-02 02:43:13 -0300

    add food counting system and spending for new ants i wanna sleeeeeeeeppp

--- scenes/food/food_parent.gd
+++ scenes/food/food_parent.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 class_name object
+@export var food_value = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- scenes/level/level_parent.gd
+++ scenes/level/level_parent.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var follower_scene = preload("res://scenes/player/follower.tscn")
+var food = 0
 
 func _physics_process(_delta):
 	$LeafParticles.global_position = $player.global_position - Vector2(0, 500)
@@ -8,16 +9,19 @@ func _on_player_spawn_ant():
 	spawn_follower()
 	
 func spawn_follower():
-	var follower = follower_scene.instantiate() as CharacterBody2D
-	$followers.call_deferred("add_child", follower)
+	if food >= 3:
+		food -= 3
+		$UI/FoodCounter.text = "Food: " + str(food)
+		var follower = follower_scene.instantiate() as CharacterBody2D
+		$followers.call_deferred("add_child", follower)
 	
-	if not $followers.get_children().is_empty():
-		var last_follower: CharacterBody2D = $followers.get_children().back()
-		follower.target_path = last_follower.get_path()
-		follower.global_position = last_follower.global_position
-	else:
-		follower.target_path = "../../player"
-		follower.global_position = $player.global_position
+		if not $followers.get_children().is_empty():
+			var last_follower: CharacterBody2D = $followers.get_children().back()
+			follower.target_path = last_follower.get_path()
+			follower.global_position = last_follower.global_position
+		else:
+			follower.target_path = "../../player"
+			follower.global_position = $player.global_position
 		
 func on_ant_circle(area):
 	if $followers.get_child_count() > 2:
@@ -37,8 +41,11 @@ func _on_player_circle_formed(area):
 
 func _on_eating_area_body_entered(body): #add points for food later
 	if body.has_method("get_eaten"):
+		food += body.food_value
+		$UI/FoodCounter.text = "Food: " + str(food)
 		$Mordida.play()
 		body.get_eaten()
+		print(food)
 		
 
 func change_player():
@@ -51,14 +58,12 @@ func change_player():
 		$followers.get_child(1).target_path = "../../player"
 		$followers.get_child(1).update_target()
 	first_follower.queue_free()
-
-func _on_player_kill_ant():
-	change_player()
-	
 	
 func _on_player_crumb_entered(area): 
 	var layer = area.get_collision_layer()
 	if layer == 4:
+		$UI/FoodCounter.text = "Food: " + str(food)
+		food += 1
 		$Mordida.play()
 		area.queue_free() #TODO crumb eaten behavior
 	elif layer == 32:
